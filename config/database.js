8f23038b02e815243905caaa9ce837f6651a8c86
App Academy Academics <academics@appacademy.io>
2020-05-04 04:12:42 -0500

    Initial import.

---
+++ config/database.js
@@ -0,0 +1,24 @@
+const config = require('./');
+
+const db = config.db;
+const username = db.username;
+const password = db.password;
+const database = db.database;
+const host = db.host;
+
+module.exports = {
+  development: {
+    username,
+    password,
+    database,
+    host,
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+  },
+  test: {
+    dialect: "sqlite",
+    DB_CONN: "sqlite.memory",
+    logging: false,
+    seederStorage: 'sequelize',
+  },
+};
